4cdf132a39863dc8815f8dcd7824e1c60561577d
David Hohl <david.hohl@gmail.com>
2022-01-14 08:54:32 +0100

    feat: emotion/cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,23 +10,30 @@ import { ThemeProvider } from '@mui/material/styles';
 
 import { useApollo } from '../apollo/client';
 import theme from '../theme/theme';
+import { createEmotionCache } from '../utilities/createEmotionCache';
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
-function MyApp({ Component, pageProps }: AppProps) {
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
+  
   return (
-    <ApolloProvider client={apolloClient}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ApolloProvider>
+    <CacheProvider value={emotionCache}>
+      <ApolloProvider client={apolloClient}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+      </CacheProvider>
   );
 }
 
